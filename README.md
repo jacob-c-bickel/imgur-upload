47b8abdb36735020571778f646a6d5743a5d84a6
Jacob Bickel <bickelja@cse.msu.edu>
2019-07-25 00:59:08 -0400

    updated client creation and user authorization, integrated arg parsing into app class

--- README.md
+++ README.md
@@ -4,24 +4,16 @@ A utility for creating Imgur albums and uploading images.
 
 ## User Setup
 
-To use this application, you must first register with Imgur and update the
-creds.json file with your Client ID and Client Secret.
+To use this application, you must first register with Imgur and provide valid client credetials.
 
-1. Register an application.
-    - While logged in to Imgur, visit https://api.imgur.com/oauth2/addclient
-    - Populate the form:
-        - `Application name`: anything
-        - `Authorization callback URL`: https://imgur.com
-        - `Email`: your email address
-        - `Description`: something like "Basic app to upload images."
-    - Click Submit.
+1. While logged in to Imgur, visit https://api.imgur.com/oauth2/addclient
+2. Populate and submit the form:
+    - `Application name`: anything
+    - `Authorization callback URL`: https://imgur.com
+    - `Email`: your email address
+    - `Description`: something like "Basic app to upload images."
+3. Copy the Client ID and Client Secret into the prompts.
 
-2. Update creds.json.
-    - After submitting, you will see your Client ID and Client Secret.
-    - Open creds.json in a text editor.
-    - Copy the Client ID and Client Secret into the appropriate places in creds.json (leave the double quotes).
-
-If you do not upload anonymously, you will be prompted with an OAuth flow on
-the first execution.
+If you do not upload anonymously, you will be prompted with an OAuth flow on the first execution.
 
 For help, run the program in a terminal with the -h option.
--- imgur_upload/app.py
+++ imgur_upload/app.py
@@ -3,7 +3,6 @@ import glob
 import json
 import os
 import sys
-import time
 import urllib
 import webbrowser
 
@@ -11,83 +10,90 @@ from imgurpython import ImgurClient
 
 dirname = os.path.dirname(__file__)
 
+
 class App:
     def __init__(self):
         self.client = None
 
 
-    def check_client_credentials(self):
-        with open(os.path.join(dirname,"creds.json"), "r") as f:
+    def initialize_client(self):
+        with open(os.path.join(dirname, "creds.json"), "r") as f:
             stored_creds = json.load(f)
-        if stored_creds["client_id"] == "YOUR_CLIENT_ID" or \
-                stored_creds["client_secret"] == "YOUR_CLIENT_SECRET":
-            print("Error: No client credentials found. ", end="")
-            print("You must first register an applicaton with Imgur. ", end="")
-            print("See README.md for help. ")
-            input("press enter to exit...")
-            sys.exit(0)
+            if "client_id" not in stored_creds:
+                stored_creds["client_id"] = None
+            if "client_secret" not in stored_creds:
+                stored_creds["client_secret"] = None
 
+        client_id = stored_creds["client_id"]
+        client_secret = stored_creds["client_secret"]
+        new_creds = False
 
-    def init_client(self, anon):
-        with open("creds.json", "r") as file_in:
-            stored_creds = json.load(file_in)
-
-        self.client = ImgurClient(
-            stored_creds["client_id"],
-            stored_creds["client_secret"]
-        )
-
-        if not anon:
-            if "refresh_token" in stored_creds:
-                # Authorize using stored creds.
-                self.client.set_user_auth(
-                    stored_creds["access_token"],
-                    stored_creds["refresh_token"]
-                )
-            else:
-                # User OAuth flow.
-                print("Please follow the OAuth flow and enter the pin...")
-                time.sleep(2)
-                webbrowser.open(self.client.get_auth_url("pin"))
-                pin = input("Enter pin: ")
-
-                # Authorize using new creds.
-                credentials = self.client.authorize(pin, "pin")
-                self.client.set_user_auth(
-                    credentials["access_token"],
-                    credentials["refresh_token"]
-                )
-
-                # Update creds file.
-                stored_creds["access_token"] = credentials["access_token"]
-                stored_creds["refresh_token"] = credentials["refresh_token"]
-                with open(os.path.join(dirname, "creds.json"), "w") as f:
-                    json.dump(stored_creds, f, indent=2)
-
-
-    def arg_form(self):
-        print("Imgur Upload\n")
-        
-        print("Targets may be a url, filepath, or directory.")
-        targets = []
         while True:
-            target = input("Target: ")
-            if not target:
+            try:
+                self.client = ImgurClient(client_id, client_secret)
                 break
-            targets.append(target)
-        print("")
+            except:
+                print("*** Missing or invalid client credentials. ", end="")
+                print("You must first register an applicaton with Imgur ", end="")
+                print("and provide the client id and secret. ", end="")
+                print("See README.md for help.")
+                client_id = input("Client ID: ")
+                client_secret = input("Client secret: ")
+                new_creds = True
 
-        title = input("Title: ")
-        print("")
 
-        description = input("Description: ")
-        print("")
+        # Update creds file if there was a change.
+        if new_creds:
+            print("Client credentials successfully validated.\n")
 
-        anon = (input("Anonymous (y/n): ") == "y")
-        print("")
+            stored_creds["client_id"] = client_id
+            stored_creds["client_secret"] = client_secret
 
-        return argparse.Namespace(targets=targets, title=title,
-                description=description, anon=anon)
+            with open(os.path.join(dirname, "creds.json"), "w") as f:
+                json.dump(stored_creds, f)
+
+
+    def authorize_user(self):
+        with open(os.path.join(dirname, "creds.json"), "r") as f:
+            stored_creds = json.load(f)
+            if "access_token" not in stored_creds:
+                stored_creds["access_token"] = None
+            if "refresh_token" not in stored_creds:
+                stored_creds["refresh_token"] = None
+
+        access_token = stored_creds["access_token"]
+        refresh_token = stored_creds["refresh_token"]
+        new_tokens = False
+        
+        while True:
+            try:
+                self.client.set_user_auth(access_token, refresh_token)
+                break
+            except:
+                print("*** Missing or invalid tokens. ", end="")
+                print("Please follow the OAuth flow and enter the pin.")
+                webbrowser.open(self.client.get_auth_url("pin"))
+                pin = input("Pin: ")
+
+                try:
+                    credentials = self.client.authorize(pin, "pin")
+                except:
+                    print("Invalid pin.")
+                    continue
+
+                access_token = credentials["access_token"]
+                refresh_token = credentials["refresh_token"]
+                new_tokens = True
+
+        # Update creds file if there was a change.
+        if new_tokens:
+            print("Tokens successfully validated.\n")
+
+            stored_creds["access_token"] = access_token
+            stored_creds["refresh_token"] = refresh_token
+
+            with open(os.path.join(dirname, "creds.json"), "w") as f:
+                json.dump(stored_creds, f)
 
 
     def create_album(self, targets, title, description):
@@ -109,22 +115,24 @@ class App:
         for target in targets:
             # URL upload.
             if urllib.parse.urlparse(target).scheme in ("http", "https"):
-                image = self.upload_image(config, url=target)
-                if image is not None:
-                    images.append(image)
+                images += filter(None, [self.upload_image(config, url=target)])
             # File upload.
             elif os.path.isfile(target):
-                image = self.upload_image(config, path=target)
-                if image is not None:
-                    images.append(image)
+                images += filter(None, [self.upload_image(config, path=target)])
             # Directory upload.
             elif os.path.isdir(target):
                 for filepath in glob.glob(f"{target}/**"):
-                    image = self.upload_image(config, path=filepath)
-                    if image is not None:
-                        images.append(image)
+                    images += filter(None,
+                            [self.upload_image(config, path=filepath)])
             else:
                 print(f"*** Invalid target: {target} ***")
+
+        print("========================================")
+
+        print(f"Album: https://imgur.com/a/{album['id']}")
+        if images:
+            print(f"First Image: https://imgur.com/{images[0]['id']}.png")
+        print("")
 
         return {
             "album_id": album["id"],
@@ -150,32 +158,8 @@ class App:
         return image
 
 
-    def run(self, args=None):
-        self.check_client_credentials()
-
-        if args is None:
-            args = self.arg_form()
-
-        self.init_client(args.anon)
-
-        res = self.create_album(args.targets, args.title, args.description)
-        print(f"\nAlbum: https://imgur.com/a/{res['album_id']}")
-        if res['image_ids']:
-            print(f"First Image: https://imgur.com/{res['image_ids'][0]}.png")
-        print("")
-
-        print(self.client.credits)
-        print("")
-
-        input("press enter to exit...")
-
-
-def main():
-    app = App()
-    if len(sys.argv) == 1:
-        app.run()
-    else:
-        parser = argparse.ArgumentParser()
+    def parse_args(self, raw_args):
+        parser = argparse.ArgumentParser(raw_args)
         parser.add_argument(
             "targets",
             metavar="target",
@@ -200,4 +184,49 @@ def main():
             action="store_const", const=True, default=False,
             help="anonymously upload (no imgur account)"
         )
-        app.run(args=parser.parse_args())
+        return parser.parse_args()
+
+
+    def prompt_args(self):
+        print("-- Targets may be a url, filepath, or directory.")
+        targets = []
+        while True:
+            target = input("Target: ")
+            if target:
+                targets.append(target)
+            else:
+                break
+
+        title = input("Title: ")
+        description = input("Description: ")
+        anon = (input("Anonymous (y/n): ") == "y")
+        print("")
+
+        return argparse.Namespace(targets=targets, title=title,
+                description=description, anon=anon)
+
+
+    def run(self, raw_args):
+        self.initialize_client()
+
+        if raw_args:
+            interactive = False
+            args = self.parse_args(raw_args)
+        else:
+            interactive = True
+            args = self.prompt_args()
+
+        if not args.anon:
+            self.authorize_user()
+
+        self.create_album(args.targets, args.title, args.description)
+
+        print(self.client.credits)
+
+        if interactive:
+            input("press enter to exit...")
+
+
+def main():
+    app = App()
+    app.run(sys.argv[1:])
